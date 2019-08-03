cec6839d93be519697ecd301e412ce7a011d711f
mrG0bliN <mrg0blin.tm@gmail.com>
2019-08-03 22:57:56 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-exnqqc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-exnqqc)
---
+++ index.js
@@ -0,0 +1,17 @@
+// матрица 10х10
+import style from style.styl;
+
+const table = `
+<table>
+ <tbody>
+  <tr>
+   <td>null</td>
+  </tr>
+ </tbody>
+</table>
+`;
+const arr1 = [];
+
+
+
+app.innerHTML = table;
